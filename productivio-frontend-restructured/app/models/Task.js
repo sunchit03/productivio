70615ff7a3a8f027e3a46714947621ca2cdac047
Sunchit Singh <sunchit333@gmail.com>
2025-01-29 00:04:16 -0500

    user and task database init with mongodb (mongoose)

---
+++ productivio-frontend-restructured/app/models/Task.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String },
+  completed: { type: Boolean, default: false },
+  dueDate: { type: Date, default: null },
+  userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+  priority: { type: String, enum: ['low', 'medium', 'high'], default: 'medium' },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Task', taskSchema);
---
+++ productivio-frontend-restructured/app/models/User.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  connection: {
+    type: String,
+    required: true,
+    enum: ['auth0', 'google-oauth2'],
+  },
+  tasks: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Task',
+    },
+  ],
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
