9cb252fd2823eac716ab665f29a835e33ad0d164
Sunchit Singh <sunchit333@gmail.com>
2025-02-06 20:42:24 -0500

    removed page auth from teams

--- productivio-frontend-restructured/app/dashboard/teams/page.jsx
+++ productivio-frontend-restructured/app/dashboard/teams/page.jsx
@@ -1,10 +1,6 @@
-import { withPageAuthRequired } from '@auth0/nextjs-auth0/client';
 
 function Teams( {user} ) {
     <h1>Teams Pageeee</h1>
 }
 
-export default withPageAuthRequired(Teams, {
-    onRedirecting: () => <Loading />,
-    onError: error => <ErrorMessage>{error.message}</ErrorMessage>
-  });
+export default Teams;
