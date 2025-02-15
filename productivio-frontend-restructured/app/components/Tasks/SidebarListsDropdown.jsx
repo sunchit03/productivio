3c3120e7f2ee335cf60e36fcd3e6e0cff3501bce
Sunchit Singh <sunchit333@gmail.com>
2025-02-15 02:58:32 -0500

    sidebar list dropdown converted to component
    using react-emoji-picker to pick emojis for list

--- productivio-frontend-restructured/app/components/Tasks/SidebarListsDropdown.jsx
+++ productivio-frontend-restructured/app/components/Tasks/SidebarListsDropdown.jsx
@@ -1,88 +1,134 @@
 import { useState } from "react";
+import { TbMoodWink2 } from "react-icons/tb";
+import EmojiPicker from 'emoji-picker-react';
+import { FaAngleRight, FaAngleDown } from "react-icons/fa6";
 import { useLists } from "../../context/ListsContext";
 
 const SidebarListsDropdown = ({ setActiveTab }) => {
   const [isListsDropdownOpen, setIsListsDropdownOpen] = useState(false);
   const [isListModalOpen, setIsListModalOpen] = useState(false);
-  const [newList, setNewList] = useState({ name: "", emoji: "📁" });
+  const [isHovered, setIsHovered] = useState(false);
+  const [newList, setNewList] = useState({ name: "", emoji: "" });
+  const [showPicker, setShowPicker] = useState(false);
   const { lists, addList, selectList } = useLists();
 
-  const openListModal = () => setIsListModalOpen(true);
-  const closeListModal = () => setIsListModalOpen(false);
+  const openListModal = () => {
+    setNewList({ name: "", emoji: "" });
+    setShowPicker(false)
+    setIsListModalOpen(true)
+  };
+  const closeListModal = () =>  setIsListModalOpen(false);
+
+  const handleEmojiClick = (emojiData, event) => {
+    setNewList({ ...newList, emoji: emojiData.emoji });
+    setShowPicker(false);
+  };
 
   const handleAddList = (e) => {
     e.preventDefault();
     if (!newList.name) return;
-    addList(newList);
-    setNewList({ name: "", emoji: "📁" });
+
+    if (!newList.emoji) {
+      addList({name: newList.name, emoji: "📁"});
+    } else {
+      addList(newList);
+    }
+
+    setNewList({ name: "", emoji: "" });
     closeListModal();
   };
 
   return (
-    <div className="relative w-full">
+    <div className="relative w-full" 
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}>
       <button
         onClick={() => setIsListsDropdownOpen(!isListsDropdownOpen)}
-        className="w-full text-left px-3 py-2 hover:bg-gray-200 flex justify-between items-center"
+        className="w-full text-left py-1 hover:bg-gradient-to-bl from-violet-100 to-fuchsia-100 hover:rounded-sm flex items-center"
       >
-        Lists
-        <span className="ml-2">{isListsDropdownOpen ? "⇧" : "⇩"}</span>
+        { isHovered ? (
+          <>
+            <span>{isListsDropdownOpen ? 
+              <FaAngleDown size={"0.75em"} className="text-black/50 hover:text-black/100"/> 
+              : 
+              <FaAngleRight size={"0.75em"} className="text-black/50 hover:text-black/100"/> }
+            </span>
+            <span className="ml-1 text-xs font-semibold">Lists</span>
+          </>
+        ):
+        <span className="ml-4 text-xs font-semibold">Lists</span>
+        }
+        
       </button>
-      <button
-        onClick={openListModal}
-        className="absolute right-3 top-2 text-black bg-white px-2 py-1 rounded-md text-sm transition-opacity duration-200"
-      >
-        +
-      </button>
+      {isHovered &&
+        <button
+          onClick={openListModal}
+          className="absolute right-3 top-0 text-black/50 hover:text-black/100 px-2 text-md"
+        >
+          +
+        </button>
+      }
       {isListsDropdownOpen && (
-        <div className="bg-white mt-1 p-2 rounded-md">
-          {lists.length > 0 ? (
-            lists.map((list) => (
-              <button
-                key={list.id}
-                onClick={() => {
-                  setActiveTab(`list-${list.id}`);
-                  selectList(list.id);
-                }}
-                className="block w-full text-left px-3 py-2 hover:bg-gray-200 rounded-md flex items-center"
-              >
-                {list.emoji} <span className="ml-2">{list.name}</span>
-              </button>
-            ))
-          ) : (
-            <p className="px-3 py-2 text-gray-400">No lists yet.</p>
-          )}
+        <div className="rounded-md">
+          <ul className="">
+            {lists.length > 0 ? (
+              lists.map((list) => (
+                <li key={list.id} className="flex relative px-[10px]">
+                  <button  
+                    onClick={() => {
+                      setActiveTab(`list-${list.id}`);
+                      selectList(list.id);
+                    }}
+                    className="w-full text-left px-3 py-2 hover:bg-gradient-to-bl from-violet-100 to-fuchsia-100 hover:rounded-sm flex items-center"
+                  >
+                    <span className="mr-[8px] w-[18px] h-[18px] flex-none">{list.emoji}</span>
+                    <p className="text-sm font-normal flex-auto">{list.name}</p>
+                  </button>
+                </li>
+              ))
+            ) : (
+              <div className="px-3 py-2 bg-gray-100 rounded-sm text-gray-400 text-xs">Use lists to categorize and manage your tasks and notes</div>
+            )}
+          </ul>
         </div>
       )}
 
       {/* Modal for adding a new list */}
       {isListModalOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
-          <div className="bg-white p-6 rounded-md shadow-lg w-80">
-            <h2 className="text-xl font-bold mb-4 text-black">Create New List</h2>
+        <div className="fixed inset-0 flex items-start justify-center bg-gray-900 bg-opacity-50 z-50 mt-50">
+          <div className="bg-white p-6 rounded-md shadow-lg w-2/5">
+            <h2 className="text-xl font-bold mb-4 text-black">New List</h2>
             <form onSubmit={handleAddList}>
               <label className="block text-black mb-2">
-                List Name:
-                <input
-                  type="text"
-                  value={newList.name}
-                  onChange={(e) => setNewList({ ...newList, name: e.target.value })}
-                  className="w-full border rounded p-2 text-black mt-1"
-                  required
-                />
-              </label>
-              <label className="block text-black mb-2">
-                Choose Emoji:
-                <select
-                  value={newList.emoji}
-                  onChange={(e) => setNewList({ ...newList, emoji: e.target.value })}
-                  className="w-full border rounded p-2 text-black mt-1"
-                >
-                  <option>📁</option>
-                  <option>🎓</option>
-                  <option>📅</option>
-                  <option>💼</option>
-                  <option>🏠</option>
-                </select>
+                <div className="relative mt-1">
+                  <span 
+                    onClick={() => setShowPicker((val) => !val)}
+                    className="absolute inset-y-0 left-0 flex items-center pl-3 cursor-pointer text-xl"
+                  >
+                    {newList.emoji === "" ? <TbMoodWink2 /> : newList.emoji}
+                  </span>
+                  <input
+                    type="text"
+                    value={newList.name}
+                    onChange={(e) => setNewList({ ...newList, name: e.target.value })}
+                    placeholder="Name"
+                    className="w-full border rounded p-2 pl-10 text-black"
+                    required
+                    autoFocus
+                  />
+                  {showPicker && (
+                    <div className="absolute z-10 mt-2 -mx-5">
+                      <EmojiPicker
+                        width={300}
+                        height={350}
+                        autoFocusSearch={false}
+                        emojiStyle="google"
+                        onEmojiClick={handleEmojiClick}
+                        previewConfig={{ showPreview: false }}
+                      />
+                    </div>
+                  )}
+                </div>
               </label>
               <div className="flex justify-end mt-4">
                 <button type="button" onClick={closeListModal} className="mr-2 px-4 py-2 bg-gray-400 text-white rounded">
