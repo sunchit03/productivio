46e8297cfd384bc46cf55034571b19506ac7ed52
Sunchit Singh <sunchit333@gmail.com>
2025-02-17 17:54:05 -0500

    matrix block group hovering panga

--- productivio-frontend-restructured/app/components/EisenhowerMatrix/MatrixBlock.jsx
+++ productivio-frontend-restructured/app/components/EisenhowerMatrix/MatrixBlock.jsx
@@ -17,7 +17,6 @@ const titleColorMap = {
 const MatrixBlock = ({tasks, title, refresh, userId}) => {
     const [addTaskModal,setAddTaskModal] = useState(false);
     const [selectedTask, setSelectedTask] = useState(null); // Track the task being edited
-    const [isVisible, setIsVisible] = useState(false);
 
       // Extract text from JSX title prop
     const textContent = title.props.children.filter(child => typeof child === "string").join(" ");
@@ -25,45 +24,49 @@ const MatrixBlock = ({tasks, title, refresh, userId}) => {
 
 
     return(
-        <div className= "border rounded-md p-2 bg-white hover:bg-gray-100 shadow-md h-70 flex flex-col"
-            onMouseEnter={() => setIsVisible(true)}
-            onMouseLeave={() => setIsVisible(false)}>
+        <div className= "group-quad border rounded-md p-2 bg-white shadow-md h-[43vh] flex flex-col">
             <div className={`text-sm p-2 font-bold flex w-full justify-between items-center ${textColor}`}>
-            <div className="flex items-center gap-2">
-                {title} 
+                <div className="flex items-center gap-2">
+                    {title} 
                 </div>
-            <div className="flex items-center gap-2">
-                {isVisible && (
-                    <button className="hover:bg-gray-300 rounded-sm" onClick={()=>{setAddTaskModal(true)}}><HiOutlinePlusSm size={20} /></button>
-                )}
-                    {addTaskModal && 
-                    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50"> 
-                        <EisenHowerTask
-                        userId={userId}                 
-                        onClose={() => {setAddTaskModal(false)}}
-                        reloadTasks={refresh}/>
+                <div className="invisible group-quad-hover:visible flex items-center gap-2">
+                    <button className="hover:bg-gray-300 rounded-sm" onClick={()=>{setAddTaskModal(true)}}>
+                        <HiOutlinePlusSm size={20} />
+                    </button>
+                    <button className="hover:bg-gray-300 rounded-sm" onClick={()=>{}}>
+                        <RxDotsHorizontal size={20} />
+                    </button>
+                </div>
+
+                {addTaskModal && 
+                <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50"> 
+                    <EisenHowerTask
+                    userId={userId}                 
+                    onClose={() => {setAddTaskModal(false)}}
+                    reloadTasks={refresh}/>
+                </div>
+                }
+
+            </div>
+            <div className="hover:overflow-y-auto relative">
+                {tasks.length > 0 ? (
+                    tasks.map(task => { return (
+                    <div className="group-task pr-2" key={task._id}>
+                        <div className={`px-3 py-2 rounded-md ${selectedTask == task ? "bg-purple-50 hover:bg-purple-50" : "hover:bg-gray-50"}`} 
+                        onClick={() => handleTaskSelection(task)}>
+                        <TaskItem task={task} />
+                        </div>
+                        <div className="h-[1px] bottom-0 bg-purple-50 group-task-hover:invisible z-10"></div>
                     </div>
-                    }
-                    
-                {isVisible && (
-                    <button className="hover:bg-gray-300 rounded-sm" onClick={()=>{}}> <RxDotsHorizontal size={20} /></button>
+                    )})
+                ) : 
+                (
+                    <>
+                        <span className="text-center text-base font-medium">No tasks</span>
+                    </>
                 )}
-                </div>
-                </div>
-                <div className={`${tasks.length === 0 ? "p-2 flex flex-1 justify-center items-center" :  "overflow-hidden hover:overflow-y-auto"}`}>
-                { tasks.length === 0 ? 
-                (<p className="text-sm text-gray-500">
-                    No tasks</p>
-                ) : (tasks.map(task => (
-                  <div className="group pr-2" key={task._id}>
-                    <div className= "px-3 py-2 rounded-md hover:bg-gray-200">
-                    <TaskItem task={task}/>
-                    </div>
-                    <div className="h-[1px] bottom-0 bg-gray-200 group-hover:invisible z-10"></div>
-                  </div>
-                    )))}
-                </div>
-              </div>
+            </div>
+        </div>
     );
 
 }
