67708aad937304a77d16f577c8a7f52e8545e3fd
Sunchit Singh <sunchit333@gmail.com>
2025-03-01 05:28:07 -0500

    single team page

---
+++ productivio-frontend-restructured/app/dashboard/teams/team/page.jsx
@@ -0,0 +1,54 @@
+// app/teams/team/page.jsx
+"use client";
+import { useState, useEffect } from "react";
+import MembersSection from "@/app/components/Teams/MembersSection";
+import TasksView from "@/app/components/Tasks/TasksView";
+import { getTeamData } from "@/app/services/teams";
+
+export default function TeamPage({ selectedTeam, setSelectedTeam, userId, membersSectionCollapse, setMembersSectionCollapse }) {
+    const [team, setTeam] = useState(null);
+    const [members, setMembers] = useState([]);
+    const [isAdmin, setIsAdmin] =useState(false);
+   
+    async function fetchTeamDetails() {
+        try {            
+            const data = await getTeamData(selectedTeam._id, userId);
+            if (data) {
+                console.log("Team details:", data);
+
+                setTeam(data);
+                setIsAdmin(data.admin._id === userId);
+                setMembers(data.members);
+            }
+        } catch (error) {
+            console.error("Error fetching team data:", error.message);
+        }
+    }
+    
+    useEffect(() => {
+        if(selectedTeam){
+            fetchTeamDetails();
+        }
+    }, [selectedTeam, userId])
+
+    return (
+        <div className={"sm:relative w-full flex h-full"}>
+            <MembersSection 
+                members={members} 
+                isAdmin={isAdmin} 
+                setSelectedTeam={setSelectedTeam} 
+                membersSectionCollapse={membersSectionCollapse}
+            />
+        <div className={`flex-grow h-full sm:w-full overflow-auto ${typeof window !== "undefined" && window.innerWidth < 639 && !membersSectionCollapse ? "bg-black/10" : "bg-white"}`}>
+            <TasksView 
+                title={selectedTeam.title} 
+                userId={userId} 
+                teamId={selectedTeam._id} 
+                setSelectedTeam={setSelectedTeam}
+                membersSectionCollapse={membersSectionCollapse}
+                setMembersSectionCollapse={setMembersSectionCollapse}
+            />
+        </div>
+    </div>
+    );
+}
