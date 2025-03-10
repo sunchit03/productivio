c2e47aa1c9629a25baba2185bb6b5c96bbae20ed
Sunchit Singh <sunchit333@gmail.com>
2025-03-10 03:33:18 -0400

    only fetching members' email, conn, pp and admin id when fetching particular team details

--- productivio-frontend-restructured/app/api/tasks/team/[teamId]/route.js
+++ productivio-frontend-restructured/app/api/tasks/team/[teamId]/route.js
@@ -3,7 +3,7 @@ import Task from "../../../../models/Task";
 
 export async function GET(req, { params }) {
     try {
-      const { teamId } = params;
+      const { teamId } = await params;
   
       if (!teamId) {
         return NextResponse.json({ success: false, error: "Team ID is required" }, { status: 400 });
