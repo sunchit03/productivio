538a84a430ee7757779c8fd54a22c3e32851f19b
agoyal31 <agoyal31@myseneca.ca>
2025-03-29 01:26:11 -0400

    Created About page

--- productivio-frontend-restructured/app/components/Home/Navbar.jsx
+++ productivio-frontend-restructured/app/components/Home/Navbar.jsx
@@ -2,17 +2,19 @@
 
 import React from 'react';
 import styles from '../../styles/Navbar.module.css';
-
+import Link from 'next/link';
 const Navbar = () => {
   return (
     <nav className={`${styles.navbar} bg-white shadow-md`}>
       <div className="container mx-auto px-6 flex justify-between items-center h-16">
         <div>
+          <Link href="/">
           <img
             src="/assets/logo.png"  // Place your image in public/assets folder
             alt="logo productivio"
             className='h-14 w-44'>
           </img>
+          </Link>
         </div>
         <ul className="flex space-x-8">
           <li className="text-purple-700 font-bold cursor-pointer hover:text-purple-900">
