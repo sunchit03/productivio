e18a857cbf3249be57c9bece5d377fa4390b405f
Sunchit Singh <sunchit333@gmail.com>
2025-03-31 19:53:27 -0400

    notifications modal work

--- productivio-frontend-restructured/app/components/NotificationItem.jsx
+++ productivio-frontend-restructured/app/components/NotificationItem.jsx
@@ -9,23 +9,23 @@ export default function NotificationItem({ notification }) {
 
     const iconType = (type) => {
         switch (type) {
-            case 'task-allocate': return <MdAssignmentAdd />
-            case 'task-deallocate': return <BiTaskX />
-            case 'team-member-addition': return <LuUserRoundPlus />
-            case 'team-member-removal': return <LuUserRoundMinus />
-            case 'team-admin-assignment': return <GrUserAdmin />
-            case 'team-deletion': return <MdOutlineGroupOff />
+            case 'task-allocate': return <MdAssignmentAdd size={"2em"} className='text-violet-400'/>
+            case 'task-deallocate': return <BiTaskX size={"2em"} className='text-violet-400'/>
+            case 'team-member-addition': return <LuUserRoundPlus size={"2em"} className='text-violet-400'/>
+            case 'team-member-removal': return <LuUserRoundMinus size={"2em"} className='text-violet-400'/>
+            case 'team-admin-assignment': return <GrUserAdmin size={"2em"} className='text-violet-400'/>
+            case 'team-deletion': return <MdOutlineGroupOff size={"2em"} className='text-violet-400'/>
         }
     }
 
     return (
         <>
             <li key={notification._id} className="group flex items-center">
-                <div className={`relative w-full px-3 py-2 flex justify-between rounded-md hover:bg-indigo-500/5`}>
+                <div className={`relative w-full px-3 py-2 flex justify-between rounded-md hover:bg-indigo-500/5 ${notification.new && "bg-indigo-50"}`}>
                     <div className="w-full flex items-center">
-                        <span>{iconType(notification.type)}</span>
+                        <span className='mr-3'>{iconType(notification.type)}</span>
                         <div className='flex flex-col'>
-                            <span className="text-black">{notification.title}</span>
+                            <span className="text-black text-sm">{notification.title}</span>
                         </div>
                     </div>
                 </div>
--- productivio-frontend-restructured/app/components/NotificationsModal.jsx
+++ productivio-frontend-restructured/app/components/NotificationsModal.jsx
@@ -1,36 +1,66 @@
 // NotificationsModal.jsx
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { IoClose  } from "react-icons/io5";
+import { GrClearOption } from "react-icons/gr";
 import { GiMicrophone } from "react-icons/gi";
 import NotificationItem from "./NotificationItem";
 
-export default function NotificationsModal({ onClose, notifications }) {
-    
-    return (
-      <div className="fixed top-2 left-16 min-h-[70%] w-80 bg-white shadow-lg rounded-lg z-50">
-        <div className="bg-white p-4 rounded h-full">
-          <div className="flex justify-evenly">
-            <div className="font-bold text-black">Notifications</div>
-          </div>
-          <div className="mt-4">
-            {notifications.length > 0 ? (
-              <ul>
-                {notifications.map((notification) => 
-                  <NotificationItem key={notification._id} notification={notification} />
-                )} 
-              </ul>
-            ):
-            <div className="flex flex-col items-center justify-center h-full text-black cursor-default">
-              <div className="flex mb-2 items-center">
-                <GiMicrophone size={"4em"} className="text-violet-300" />   
-              </div>
-              <span className="text-center text-base font-medium">No notifications</span>
-              <span className="text-center text-xs font-thin">Team related messages will appear here</span>    
+export default function NotificationsModal({ onClose, notifications, readNotifications, clearNotifications }) {
+  const [modalNotifications, setModalNotifications] = useState([]);
 
-            </div>
-            }
+  useEffect(() => {
+    setModalNotifications(notifications);
+  }, [notifications.length])
+
+
+  const handleNotificationsClear = () => {
+    clearNotifications();
+    setModalNotifications([]);
+  }
+
+  const handleModalClose = () => {
+    if (modalNotifications.some(notification => notification.new)) {
+      readNotifications();
+    }
+    onClose();
+  }
+
+  return (
+    <div className="fixed top-2 left-16 h-[75%] w-80 bg-white shadow-xl rounded-lg z-50">
+      <div className="bg-white p-4 rounded h-full">
+        <div className="flex justify-between">
+          <div className={`p-1 hover:bg-gray-100 hover:rounded-md`}>
+            <IoClose size={"1.5em"} className={`text-gray-400 font-thin cursor-pointer`} onClick={handleModalClose} />
           </div>
-          <button className="absolute inset-x-0 bottom-0 w-full p-2 border rounded text-black" onClick={onClose}>Close</button>
+          <div className="font-bold text-black">Notifications</div>
+          <div className={`p-1 hover:bg-gray-100 hover:rounded-md`}>
+            <GrClearOption size={"1.3em"} 
+              title="Clear All"
+              className={`text-gray-400 font-thin ${modalNotifications.length === 0 ? 'cursor-not-allowed' : 'cursor-pointer'}`} 
+              onClick={handleNotificationsClear} 
+            />
+          </div>
+        </div>
+        <div className="mt-4 mb-2 h-[85%] lg:overflow-y-auto overflow-y-hidden hover:overflow-y-auto">
+          {modalNotifications.length > 0 ? (
+              modalNotifications.map((notification) => (
+                <div className="group" key={notification._id}>
+                  <NotificationItem key={notification._id} notification={notification} />
+                  <div className={`h-[1px] bottom-0 group-hover:invisible z-10 bg-purple-50`}></div>
+                </div>
+              ))
+          ):
+          <div className="flex flex-col items-center justify-center h-full text-black cursor-default">
+            <div className="flex mb-2 items-center">
+              <GiMicrophone size={"4em"} className="text-violet-300" />   
+            </div>
+            <span className="text-center text-base font-medium">No notifications</span>
+            <span className="text-center text-xs font-thin">Team related messages will appear here</span>    
+
+          </div>
+          }
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
